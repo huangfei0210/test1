ef84e234dc5995c68067be27ed6187a52de76e81
黄翡 <784230442@qq.com>
2017-08-17 08:40:16 +0800

    test.php提交

---
+++ test.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+    tr{
+    	text-align: center;
+    }
+	</style>
+</head>
+<body>
+<form action="123.html" method="post">
+   <table border="1" width="200" align="center" cellspacing="0" cellpadding="0">
+	<tr>
+		<td>姓名</td>
+		<td>黄翡</td>
+	</tr>
+	<tr>
+		<td>年龄</td>
+		<td>25</td>
+	</tr>
+	<tr>
+		<td>性别</td>
+		<td>男</td>
+	</tr>
+	</table>
+</form>	
+</body>
+</html>
